38d303c8d1d754f73f65b6d002675e0b72b1ddc8
Badhon Barman <58289031+BadhonBarman@users.noreply.github.com>
2021-01-05 13:49:51 +0600

    dumping some useless line

--- sketch.py
+++ sketch.py
@@ -1,14 +1,3 @@
-# import cv2
-
-# img_location = 'C:/Users/Badhon Barman/Desktop'
-# filename = 'my3.jpg'
-
-# img = cv2.imread(img_location+filename)
-
-# cv2.imshow('Original Image', img)
-# cv2.waitKey(0)
-
-
 import cv2
 
 image=cv2.imread("C:/Users/Badhon Barman/Desktop/import.jpg")
@@ -19,4 +8,3 @@ invertedblur=255-blurred
 pencilsketch = cv2.divide(gray_image, invertedblur, scale = 245.0)
 
 cv2.imwrite("C:/Users/Badhon Barman/Desktop/save.jpg", pencilsketch)
-
